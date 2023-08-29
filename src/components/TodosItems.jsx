ef9aa67e6cfe39c07c652888e82dfe2feaf71ec9
unknown <zemigz007@gmail.com>
2023-08-29 21:54:43 +0800

    adding alt value cross

--- src/components/TodosItems.jsx
+++ src/components/TodosItems.jsx
@@ -4,7 +4,7 @@ const TodoItems = ({id,title,completed,handleTodoToggle,handleDelete}) => {
           <input className="border-2 group-hover:border-[#7323dc] border-[#d7d7d7] dark:border-slate-500 dark:bg-transparent shrink-0 h-5 w-5 rounded-full outline-none accent-[#7323dc] cursor-pointer" type="checkbox" onChange={(e)=>handleTodoToggle(id,e.target.checked)} checked={completed} />
           <p className={`mr-auto text-sm truncate ${completed?'line-through':''}`}>{title}</p>
           <button className="shrink-0 hidden group-hover:block" onClick={()=>handleDelete(id)}>
-            <img src="/assets/icon-cross.svg" alt="" />
+            <img src="./assets/icon-cross.svg" alt="cross" />
           </button>
         </li>
         
