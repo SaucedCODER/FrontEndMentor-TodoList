eb3a2c3adc077b3556be46f3ddcb5f7904c5aedf
unknown <zemigz007@gmail.com>
2023-08-29 21:04:10 +0800

    ...

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/FrontEndMentor-TodoList/",
   plugins: [react()],
-  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", "**/*.svg"],
 });
